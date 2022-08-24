0daeca02c35d853aa3b13871a56929e1a7dad644
Nguyễn Hồng Quang <90305443+Ngquang@users.noreply.github.com>
2022-08-24 09:54:13 +0700

    Add files via upload

---
+++ theme_tutorial/__manifest__.py
@@ -0,0 +1,51 @@
+{
+    'name': "Viindoo Theme Tutorial",
+    'name_vi_VN': "",
+
+    'summary': """
+Module tutorial for theme
+""",
+
+    'summary_vi_VN': """
+
+    	""",
+
+    'description': """
+
+ 
+Editions Supported
+==================
+1. Community Edition
+2. Enterprise Edition
+
+    """,
+
+    'description_vi_VN': """
+Mô đun này thay đổi một vài thông tin dành riêng cho thương hiệu Viindoo
+
+Ấn bản được Hỗ trợ
+==================
+1. Ấn bản Community
+2. Ấn bản Enterprise
+
+    """,
+
+    'author': "Viindoo",
+    'website': "https://www.tvtmarine.com",
+    'live_test_url': "https://v14demo-int.viindoo.com",
+    'live_test_url_vi_VN': "https://v14demo-vn.viindoo.com",
+    'support': "support@ma.tvtmarine.com",
+    'category': 'Theme',
+    'version': '0.1',
+    'depends': ['website'],
+    'data' : [
+        'views/website_template.xml',
+        'views/page.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 99.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
